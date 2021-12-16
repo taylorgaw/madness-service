1d041fbf43e301794394d0f9f2d79922bf8ad5ff
Taylor Gaw <taylorgaw@gmail.com>
2021-12-15 17:32:38 -0800

    Adding basic CRUD functionality for user, school and game. Update README to reflect current changes

--- README.md
+++ README.md
@@ -1,29 +1,31 @@
 # Madness Service
 
 ## Current State - 11/30/21
-1. **Game Builder** (Medium) - In Progress
-  * Generate/Build board json
-	- [x] Enter 16 player names
-	- [x] Generate JSON representation of game to send to UI
-	- [ ] Validate Gameboard sent from UI
+1. **Dockerize It** (Medium Effort) 
+   - [x] Implement MySQL
+   - [x] Implement Docker-Compose
+2. **Implement Flask W/ Basic Endpoints** 
+   - [x] Create Endpoints for basic user, game, school CRUD
 
 
 ## TO-DO:
-1. **Dockerize It** (Heavy Effort) 
-   [x] Implement MySQL
-   [x] Implement Docker-Compose
-   [ ] Generalize utility functions to connect to docker and query db on start up
-   [ ] Add API endpoints Flask app.py
+1. **Game Builder** (Medium) - In Progress
+  * Generate/Build board json
+	- [ ] Generate JSON representation of game to send to UI
+	- [ ] Set Pick Owner as User ID when stored, and replace ID with name when sent to front end
+   - [ ] Given Regions and Seeds, autofill game board
 
-2. **Database Models** (Small Effort) 
-   [x] Create Models for Users, Gameboards, and schools
- 
+2. **Database Functionality** (Medium Effort) 
+   - [ ] Implement SQLAlchemy
+     - Make use of it returning IDs for Inserts. Use IDs to create user/game link
+   - [x] Create Models for Users, Gameboards, and schools
+
 3. **SuperAdmin and Admin Functionality Logic** (Small effort)
-   [ ] Manage users (add, edit, remove users)
-   [ ] Manage picks (add, edit, remove picks)
-   [ ] SuperAdmin inherits Admin functionality plus:
-   *  [ ] Access all users (CRUD)
-   *  [ ] Access all games (CRUD)
+   - [ ] Manage users (add, edit, remove users)
+   - [ ] Manage picks (add, edit, remove picks)
+   - [ ] SuperAdmin inherits Admin functionality plus:
+   *  - [ ] Access all users (CRUD)
+   *  - [ ] Access all games (CRUD)
 
 4. **Test Suite** (Heavy Effort)
    [ ] Load mock data into MySQL
@@ -39,6 +41,7 @@
   * Owner
   * Users
   * Picks (JSON)
+  * Active
 * User
   * Name
   * Email
@@ -54,20 +57,25 @@
 ```
 
 ## Next Steps
+* **Clean Up Code**
+ - [ ] [Make Flask App More Production Ready](https://mark.douthwaite.io/getting-production-ready-a-minimal-flask-app/)
+ - [ ] [Update Dockerfile to wait for MySQL](https://www.datanovia.com/en/lessons/docker-compose-wait-for-container-using-wait-tool/docker-compose-wait-for-mysql-container-to-be-ready/)
+ - [ ] Generalize utility functions to connect to docker and query db on start up
+
 * **AWS Integration** (Heavy Effort)
-  [ ] Implement localstack (dynamodb, s3 [to store db data])
-  [ ] Implement serverless
-  [ ] Create api-spec.yaml
-  [ ] Create Models for User/Game requests/responses
+- [ ] Implement localstack (dynamodb, s3 [to store db data])
+- [ ] Implement serverless
+- [ ] Create api-spec.yaml
+- [ ] Create Models for User/Game requests/responses
 
 * **Score Service** (Medium Effort) 
-   * Create scraper to pulls scores and odds from KenPom (or some other college baskeball website) and update the scores and odds stored in a cache using [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
-   * Create scraper to get the info of teams that are in the tournament 
-   * Create Lambda running on cron to trigger score scraper during tournament times and 
+ - [ ] Create scraper to pulls scores and odds from KenPom (or some other college baskeball website) and update the scores and odds stored in a cache using [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+ - [ ] Create scraper to get the info of teams that are in the tournament 
+ - [ ] Create Lambda running on cron to trigger score scraper during tournament times and 
     
 * **Score Cache** (Small Effort)
-   * Simple DyanmoDB or Redis Cache
-   * Stores latest results from games being played
+ - [ ] Simple DyanmoDB or Redis Cache
+ - [ ] Stores latest results from games being played
 
 * Probability to Win % per player on board
 
--- api/db/utils.py
+++ api/db/utils.py
@@ -1,17 +1,179 @@
 '''
 Connection functions to create session with db
 '''
+import json
+
 import mysql.connector
 
-
-def db_init():
-    mydb = mysql.connector.connect(
+mysql_db = mysql.connector.connect(
         host='mysqldb',
         user='root',
         password='p@ssw0rd1',
         database='madness'
     )
-    cursor = mydb.cursor()
 
-    cursor.execute()
+# sqlalchemy.create_engine('mysql+pymysql://root:p@ssw0rd1@mysqldb/madness?charset=utf8mb4')
 
+CREATE_USER_STATEMENT_TEMPLATE = '''INSERT INTO user (name, email, password) VALUES ('{name}', '{email}', '{password}');'''
+CREATE_GAME_STATEMENT_TEMPLATE = '''INSERT INTO game (title, owner, picks) VALUES ('{title}', {owner}, '{picks}');'''
+CREATE_SCHOOL_STATEMENT_TEMPLATE = '''INSERT INTO school (name, mascot, initials) VALUES ('{name}', '{mascot}', '{initials}');'''
+DELETE_STATEMENT_TEMPLATE = '''DELETE FROM {table} WHERE id = {id};'''
+
+# User CRUD methods
+def get_all_users():
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute('SELECT * FROM user;')
+        row_headers = [x[0] for x in cursor.description]
+        results = cursor.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
+
+        cursor.close()
+
+        return json.dumps(json_data, default=str)
+    except Exception as e:
+        return e.message
+
+
+def get_user_by_id(id: int):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(f'SELECT * FROM user WHERE id = {id};')
+        row_headers = [x[0] for x in cursor.description]
+        result = cursor.fetchone()
+        cursor.close()
+        return dict(zip(row_headers, result))
+    except Exception as e: 
+        return str(e)
+
+
+def get_users_by_game_id(game_id: int):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(f'SELECT u.name FROM user_game_match ug JOIN user AS u ON u.id == ug.user_id WHERE ug.user_id = {game_id};')
+        # TODO: Figure out how best to return these
+        return ''
+    except Exception as e:
+        return str(e)        
+
+
+def create_user(name:str, password: str, email: str):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(CREATE_USER_STATEMENT_TEMPLATE.format(name=name, password=password, email=email))
+        cursor.close()
+    except Exception as e:
+        return e
+
+
+def delete_user_by_id(id: int):
+    try:
+        cursor = mysql_db.cursor()
+        cursor.execute(DELETE_STATEMENT_TEMPLATE.format(table='user', id=id))
+        cursor.close()
+    except Exception as e:
+        return e
+
+
+# Game CRUD methods
+def get_all_games():
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute('SELECT * FROM game;')
+        row_headers = [x[0] for x in cursor.description]
+        results = cursor.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
+
+        cursor.close()
+
+        return json.dumps(json_data, default=str)
+    except Exception as e:
+        return e.message
+
+
+def get_game_by_id(id: int):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(f'SELECT * FROM game WHERE id = {id};')
+        row_headers = [x[0] for x in cursor.description]
+        result = cursor.fetchone()
+        cursor.close()
+        return dict(zip(row_headers, result))
+    except Exception as e: 
+        return str(e)
+
+
+def get_games_by_user_id(user_id: int):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(f'SELECT g.title FROM user_game_match AS ug JOIN game AS g ON g.id == ug.game_id WHERE ug.user_id = {user_id};')
+        # TODO: Figure out how best to return these
+    except Exception as e:
+        return str(e)
+
+
+def create_game(title: str, owner: int, picks: dict):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(CREATE_GAME_STATEMENT_TEMPLATE.format(title=title, owner=owner, picks=picks))
+    except Exception as e:
+        return e
+
+
+def delete_game_by_id(id: int):
+    try:
+        cursor = mysql_db.cursor()
+        cursor.execute(DELETE_STATEMENT_TEMPLATE.format(table='game', id=id))
+        cursor.close()
+    except Exception as e:
+        return e
+
+
+# School CRUD methods
+def get_all_schools():
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute('SELECT * FROM school;')
+        row_headers = [x[0] for x in cursor.description]
+        results = cursor.fetchall()
+        json_data = []
+        for result in results:
+            json_data.append(dict(zip(row_headers, result)))
+
+        cursor.close()
+
+        return json.dumps(json_data, default=str)
+    except Exception as e:
+        return e.message
+
+
+def create_school(name: str, mascot: str, initials: str):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(CREATE_SCHOOL_STATEMENT_TEMPLATE.format(name=name, mascot=mascot, initials=initials))
+    except Exception as e:
+        return str(e)
+
+def get_school_by_id(id: int):
+    cursor = mysql_db.cursor()
+    try:
+        cursor.execute(f'SELECT * FROM school WHERE id = {id};')
+        row_headers = [x[0] for x in cursor.description]
+        result = cursor.fetchone()
+        cursor.close()
+        return dict(zip(row_headers, result))
+    except Exception as e: 
+        return str(e)
+
+
+def delete_school_by_id(id: int):
+    try:
+        cursor = mysql_db.cursor()
+        cursor.execute(DELETE_STATEMENT_TEMPLATE.format(table='school', id=id))
+        cursor.close()
+    except Exception as e:
+        return e
